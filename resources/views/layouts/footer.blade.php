573eb20464c01887c55a761ac3903c4f21987c68
AICSEMICON_MY\kamizan_hidir <kamizan980505@gmail.com>
2024-02-09 19:20:22 +0800

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- Main Footer -->
+
+<footer class="main-footer">
+
+    <strong>&copy; 2024-01-31 to {{now()}} | <a href="https://github.com/KamizanAmir" target="_blank">Kamizan Amirudin</a> |</strong><strong> All
+    rights
+    reserved.</strong>
+    <strong class="float-right">
+
+        Designed and developed by:
+        Kamizan Amirudin
+        (kamizan_hidir@aicsemicon.com)
+    </strong>
+</footer>
